4504d40e0ca7ad81c7e1d2a332727d3038de5bdc
Marcos <vinicius-mvss@live.com>
2023-02-05 01:22:25 -0300

    Coloquei a entrada de dados do usuriariono código.

--- IMC.java
+++ IMC.java
@@ -1,11 +1,13 @@
 package calculadoraIMC;
-
+import java.util.Scanner;
 public class IMC {
 	public static void main(String[] args) {
-		
-		int peso = 110;
-		double altura = 1.74;
-		double imc = peso/(altura*altura);		
+	Scanner entrada = new Scanner(System.in);	
+	System.out.println("Qual sua altura?" );
+	double altura = entrada.nextDouble();
+	System.out.println("Qual seu peso? ");
+	int peso = entrada.nextInt();
+	double imc = peso/(altura*altura);		
 			if (imc<25)
 				System.out.println("Você está no peso certo");
 			else
